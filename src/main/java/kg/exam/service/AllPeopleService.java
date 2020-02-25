7d2b4ad454a45c004fa67b270fabd92b88473c50
ERERkin <erkin1399@gmail.com>
2020-02-25 21:12:45 +0600

    Тут проблема с перегрузкой она не работает закину на всякий

--- src/main/java/kg/exam/service/AllPeopleService.java
+++ src/main/java/kg/exam/service/AllPeopleService.java
@@ -23,9 +23,54 @@ public class AllPeopleService {
     public List<Person> getSomePeople(@PathParam("name") String name, @PathParam("year") Integer year, @PathParam("gender") String gender) {
         Person person = Person.builder()
                 .name(name).year(year).gender(gender).build();
+        System.out.println(name + " " + year + " " + gender);
         PersonDao personDao = new PersonDao();
         return personDao.getAllPersons(person);
     }
+    /*@GET
+    @Path("/{name}&{year}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Person> getSomePeople(@PathParam("name") String name, @PathParam("year") Integer year) {
+        Person person = Person.builder()
+                .name(name).year(year).build();
+        PersonDao personDao = new PersonDao();
+        return personDao.getAllPersons(person);
+    }
+    @GET
+    @Path("/{name}&{gender}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Person> getSomePeople(@PathParam("name") String name,  @PathParam("gender") String gender) {
+        Person person = Person.builder()
+                .name(name).gender(gender).build();
+        PersonDao personDao = new PersonDao();
+        return personDao.getAllPersons(person);
+    }
+    @GET
+    @Path("/{year}&{gender}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Person> getSomePeople(@PathParam("year") Integer year, @PathParam("gender") String gender) {
+        Person person = Person.builder()
+                .year(year).gender(gender).build();
+        PersonDao personDao = new PersonDao();
+        return personDao.getAllPersons(person);
+    } @GET
+    @Path("/{name}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Person> getSomePeople(@PathParam("name") String name) {
+        Person person = Person.builder()
+                .name(name).build();
+        PersonDao personDao = new PersonDao();
+        return personDao.getAllPersons(person);
+    }
+    @GET
+    @Path("/{year}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Person> getSomePeople(@PathParam("year") Integer year) {
+        Person person = Person.builder()
+                .year(year).build();
+        PersonDao personDao = new PersonDao();
+        return personDao.getAllPersons(person);
+    }*/
     @DELETE
     @Path("/{personId}")
     @Consumes(MediaType.APPLICATION_JSON)
